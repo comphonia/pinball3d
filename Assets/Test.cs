a5be7d7ce4d9abb37a348867a5656bf04acd5084
Looooord <41568059+Looooord@users.noreply.github.com>
2019-03-05 07:50:04 +0100

    Paddles got better, but there's still the bug

---
+++ Assets/Test.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    HingeJoint hinge;
+
+    [SerializeField] float restPosition = 0f;
+    [SerializeField] float pressedPosition = 45f;
+    [SerializeField] float hitStrenght = 10000f;
+    [SerializeField] float flipperDamper = 150f;
+
+    [Space]
+
+    [SerializeField] string inputName;
+
+    private void Start()
+    {
+        hinge = GetComponent<HingeJoint>();
+        hinge.useSpring = true; 
+    }
+
+    private void Update()
+    {
+        JointSpring spring = new JointSpring();
+        spring.spring = hitStrenght;
+        spring.damper = flipperDamper;
+
+        if (Input.GetButton(inputName)) spring.targetPosition = pressedPosition;
+        else spring.targetPosition = restPosition;
+        hinge.spring = spring;
+        hinge.useLimits = true; 
+    }
+}
